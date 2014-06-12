b374e35268fd8293802c57a92e307e5b85e33f3d
Abe <plazith@gmail.com>
2014-06-12 22:13:46 +0000

    increased the population

--- cybertronians.py
+++ cybertronians.py
@@ -1,13 +1,14 @@
 
 class Transformer(object):
-    def __init__(self, name, vehicle_mode):
+    def __init__(self, name, vehicle_mode, voice_actor):
         self.vehicle_mode = vehicle_mode
         self.name = name
+	self.voice_actor = voice_actor
 
     def results(self):
         print self.name
         print self.get_team()
-        print "{0}\n{1}".format(self.vehicle_mode, self.morality)
+        print "{0}\n{1}\n{2}".format(self.vehicle_mode, self.morality, self.voice_actor)
 
     def get_team(self):
       """Return the name of the instance class
@@ -28,15 +29,33 @@ class Decepticon(Transformer):
 class Autobot(Transformer):
     morality = "good"
 
-bumblebee = Autobot("Bumblebee", "Volkswagon Beetle")
-starscream = Decepticon("Starscream", "F-15 Eagle")
-shockwave = Decepticon("Shockwave", "Mazda RX-8")
-ratchet = Autobot("Ratchet", "Ambulance")
+bumblebee = Autobot("Bumblebee", "Volkswagon Beetle", "Dan Gilvezan")
+starscream = Decepticon("Starscream", "F-15 Eagle", "Christopher Collins")
+shockwave = Decepticon("Shockwave", "Mazda RX-8", "Corey Burton")
+ratchet = Autobot("Ratchet", "Ambulance", "Don Messick")
+brawl = Decepticon("Brawl", "Leopard 1", "Tony St James")
+jazz = Autobot("Jazz", "Porsche 935", "Scatman Crothers")
+cliffjumper = Autobot("Cliffjumper", "Porsche 924", "Casey Kasem")
+bonecrusher = Decepticon("Bonecrusher", "Bulldozer", "Neil Ross")
+ironhide = Autobot("Ironhide", "Nissan C20 Vanette", "Peter Cullen")
+breakdown = Decepticon("Breakdown", "Lamborghini Countach", "Alan Oppenheimer")
 
 bumblebee.results()
-print "\n"
+print
 starscream.results()
-print '\n'
+print
 shockwave.results()
-print '\n'
+print
 ratchet.results()
+print
+brawl.results()
+print
+jazz.results()
+print
+cliffjumper.results()
+print
+bonecrusher.results()
+print
+ironhide.results()
+print
+breakdown.results()
